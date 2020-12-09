a92520648a378593819666fa5cb512f808bbcb28
InfiniAccess <sgschreuder@gmail.com>
2020-12-08 23:49:38 -0600

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
